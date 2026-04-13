75e2c50157af882ac3f2f44874bf748f518951c0
rahullpanditaa <rpandita1999@gmail.com>
2026-04-13 07:34:19 +0530

    modify app,py - fetch the html of input landing page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import requests
 
 app = Flask(__name__)
 
@@ -17,4 +18,24 @@ def generate():
                                message="Missing add text or URL", 
                                code=400)
     
+    # Send http request to fetch page
+    try:
+        response = requests.get(url,
+                                {"User-Agent": "Mozilla/5.0"},
+                                timeout=5)
+        if response.status_code != 200:
+            return render_template("error.html", 
+                                   message="Failed to fetch page. Try another URL",
+                                   code=response.status_code)
+        
+        # Get html 
+        html = response.text
+
+        # Return the html fetched
+        return html
+    except Exception as e:
+        return render_template("error.html",
+                               message=f"Error fetching page: {str(e)}",
+                               code=403)
+
     return render_template("generated.html", ad_text=ad_text, url=url)
